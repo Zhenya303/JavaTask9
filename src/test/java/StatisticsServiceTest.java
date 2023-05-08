467e5a13d5eeb2c777b9af7cf4a628ffec0a9cb8
“Zhenya” <dyadicheve@mail.ru>
2023-05-08 11:20:41 +0300

    com3

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -4,21 +4,19 @@ import ru.netology.statistic.StatisticsService;
 
 public class StatisticsServiceTest {
 
-        @Test
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-        void findMax() {
-            StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-            long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
-            long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
 
-            Assertions.assertEquals(expected, actual);
-        }
-
-   /* @Test
-
+    @Test
     void findMaxAnother() {
         StatisticsService service = new StatisticsService();
 
@@ -28,8 +26,8 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
-    }*/
     }
+}
 
 
 
